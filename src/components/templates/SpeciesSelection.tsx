3b60994a99bc5a506f644f9e41f9fad0770353dc
Thomas Carreau <thomas.carreau@reseau.eseo.fr>
2025-11-25 14:49:17 +0100

    Ajout du bouton de validation de sélection

--- src/components/templates/SpeciesSelection.tsx
+++ src/components/templates/SpeciesSelection.tsx
@@ -6,6 +6,7 @@ import { SPECIES } from "../../data/species";
 
 import Box from "../atoms/Box";
 import Title from "../atoms/Title";
+import PrimaryButton from "../atoms/PrimaryButton";
 
 const SPECIES2 = [...SPECIES, ...SPECIES, ...SPECIES, ...SPECIES, ...SPECIES, ...SPECIES, ...SPECIES, ...SPECIES, ...SPECIES, ...SPECIES];
 
@@ -18,12 +19,18 @@ export default function SpeciesSelection({
     <main className="h-screen w-screen flex flex-col items-center text-white p-4 bg-[url(/background_scale.png)] bg-cover overflow-auto">
       <Title name="Sélection de l'espèce" />
       <div className="size-9/10 flex flex-row gap-8 justify-center">
-        <Box className="w-2/5 flex gap-4 flex-wrap flex-start content-start">
+        <Box className="w-2/5 flex gap-4 flex-wrap flex-start content-start relative">
           {SPECIES2.map((species) => (
             <Box key={species.name} className="size-[5em]">
               
             </Box>
           ))}
+
+          <div className="absolute bottom-4 left-1/2 -translate-x-1/2">
+            <PrimaryButton onClick={() => setSpecies(SPECIES[0])}>
+              Choisir cette espèce
+            </PrimaryButton>
+          </div>
         </Box>
         
         <Box className="w-3/5 flex flex-col gap-4 overflow-auto">
@@ -32,7 +39,6 @@ export default function SpeciesSelection({
             alt={SPECIES[0].name}
             className="rounded-lg"
           />
-        
             <div>
               <h2 className="text-2xl font-bold mb-2">{SPECIES[0].name}</h2>
               <p>{SPECIES[0].description}</p>
