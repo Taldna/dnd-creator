4dcb5438183b71588f11e566b1d1182470230a8d
Simon Bihais <simon.bihais@coexya.eu>
2025-11-25 14:53:17 +0100

    Création de la page des caractéristiques

---
+++ src/components/templates/AbilitiesSelection.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import type { Ability } from "../../types/data/ability"
+
+export default function AbilitiesSelection({
+  setAbilities,
+}: {
+  setAbilities: (abilities: Ability) => void
+}) {
+  return <div>Abilities Selection for future implementation</div>
+}
--- src/pages/CharacterBuilder.tsx
+++ src/pages/CharacterBuilder.tsx
@@ -9,11 +9,14 @@ import type { Species } from "../types/data/species"
 import ClassSelection from "../components/templates/ClassSelection"
 import BackgroundSelection from "../components/templates/BackgroundSelection"
 import SpeciesSelection from "../components/templates/SpeciesSelection"
+import type { Ability } from "../types/data/ability"
+import AbilitiesSelection from "../components/templates/AbilitiesSelection"
 
 export default function CharacterBuilder() {
     const [dndClass, setDndClass] = useState<Class | null>(null)
     const [background, setBackground] = useState<Background | null>(null)
     const [species, setSpecies] = useState<Species | null>(null)
+    const [abilities, setAbilities] = useState<Ability | null>(null)
 
     if (dndClass === null) {
         return( <ClassSelection setDndClass={setDndClass} />)
@@ -21,6 +24,8 @@ export default function CharacterBuilder() {
         return( <BackgroundSelection setBackground={setBackground} />)
     } else if (species === null) {
         return( <SpeciesSelection setSpecies={setSpecies} />)
+    } else if( abilities === null) {
+        return( <AbilitiesSelection setAbilities={setAbilities} />)
     }
 
     return (
