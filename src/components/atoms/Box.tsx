e3c823fec185a0c36d7a86184d37c291e99a1e12
Thomas Carreau <thomas.carreau@reseau.eseo.fr>
2025-12-02 14:58:45 +0100

    Correction de la bordure du composant box

--- src/components/atoms/Box.tsx
+++ src/components/atoms/Box.tsx
@@ -15,307 +15,36 @@ export default function Box({
         className="absolute inset-0 w-full h-full pointer-events-none"
         fill="none"
         preserveAspectRatio="none"
-        viewBox="0 0 1024 532"
+        viewBox="0 0 1031 538"
       >
         <g id="Frame 1">
-          <line
-            id="Line 32"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="40.0922"
-            x2="19.0922"
-            y1="3.08621"
-            y2="23.0862"
-          />
-          <line
-            id="Line 27"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="357.058"
-            x2="39.0578"
-            y1="3.5"
-            y2="3.5"
-          />
-          <line
-            id="Line 25"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="363.997"
-            x2="348.997"
-            y1="18.0607"
-            y2="3.06066"
-          />
-          <line
-            id="Line 28"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="668.058"
-            x2="357.058"
-            y1="3.5"
-            y2="3.50006"
-          />
-          <line
-            id="Line 26"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="985.058"
-            x2="668.058"
-            y1="3.5"
-            y2="3.50297"
-          />
-          <line
-            id="Line 31"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="1005.02"
-            x2="984.023"
-            y1="23.0862"
-            y2="3.08621"
-          />
-          <line
-            id="Line 46"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="662.997"
-            x2="677.997"
-            y1="17.9393"
-            y2="2.93934"
-          />
-          <line
-            id="Line 22"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="524.382"
-            x2="515.189"
-            y1="17.0607"
-            y2="26.253"
-          />
-          <line
-            id="Line 33"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="508.118"
-            x2="517.053"
-            y1="17.0607"
-            y2="25.9956"
-          />
-          <line
-            id="Line 23"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="363.058"
-            x2="664.058"
-            y1="17.5"
-            y2="17.5033"
-          />
-          <line
-            id="Line 1"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="19.5578"
-            x2="19.5578"
-            y1="250"
-            y2="22"
-          />
-          <line
-            id="Line 20"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="1004.56"
-            x2="1004.56"
-            y1="22"
-            y2="250"
-          />
-          <line
-            id="Line 4"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="1.1816"
-            x2="19.6129"
-            y1="268.523"
-            y2="248.971"
-          />
-          <line
-            id="Line 45"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="1004.11"
-            x2="1022.73"
-            y1="248.909"
-            y2="266.462"
-          />
-          <line
-            id="Line 44"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="1022.58"
-            x2="1004.15"
-            y1="264.437"
-            y2="283.989"
-          />
-          <line
-            id="Line 3"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="19.6495"
-            x2="1.02891"
-            y1="284.051"
-            y2="266.498"
-          />
-          <line
-            id="Line 2"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="19.5578"
-            x2="19.5578"
-            y1="513"
-            y2="283"
-          />
-          <line
-            id="Line 17"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="1004.56"
-            x2="1004.56"
-            y1="283"
-            y2="513"
-          />
-          <line
-            id="Line 50"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="522.261"
-            x2="513.326"
-            y1="517.996"
-            y2="509.061"
-          />
-          <line
-            id="Line 49"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="505.997"
-            x2="515.19"
-            y1="517.996"
-            y2="508.803"
-          />
-          <line
-            id="Line 40"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="19.0922"
-            x2="40.0922"
-            y1="511.914"
-            y2="531.914"
-          />
-          <line
-            id="Line 34"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="984.023"
-            x2="1005.02"
-            y1="531.914"
-            y2="511.914"
-          />
-          <line
-            id="Line 51"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="666.058"
-            x2="365.058"
-            y1="517.556"
-            y2="517.553"
-          />
-          <line
-            id="Line 48"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="366.118"
-            x2="351.118"
-            y1="517.117"
-            y2="532.117"
-          />
-          <line
-            id="Line 47"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="665.118"
-            x2="680.118"
-            y1="516.996"
-            y2="531.996"
-          />
-          <line
-            id="Line 39"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="39.0577"
-            x2="356.058"
-            y1="531.5"
-            y2="531.497"
-          />
-          <line
-            id="Line 37"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="356.058"
-            x2="667.058"
-            y1="531.5"
-            y2="531.5"
-          />
-          <line
-            id="Line 35"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="667.058"
-            x2="985.058"
-            y1="531.5"
-            y2="531.5"
-          />
-          <line
-            id="Line 52"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="522.261"
-            x2="513.326"
-            y1="517.996"
-            y2="509.061"
-          />
-          <line
-            id="Line 53"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="505.997"
-            x2="515.19"
-            y1="517.996"
-            y2="508.803"
-          />
-          <line
-            id="Line 54"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="666.058"
-            x2="365.058"
-            y1="517.556"
-            y2="517.553"
-          />
-          <line
-            id="Line 55"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="366.118"
-            x2="351.118"
-            y1="517.117"
-            y2="532.117"
-          />
-          <line
-            id="Line 56"
-            stroke="var(--stroke-0, white)"
-            strokeWidth="3"
-            x1="665.118"
-            x2="680.118"
-            y1="516.996"
-            y2="531.996"
-          />
-        </g>
+            <line id="Line 94" stroke="var(--stroke-0, white)" strokeWidth="3" x1="1008.09" x2="987.087" y1="21.0862" y2="1.08621" />
+            <line id="Line 95" stroke="var(--stroke-0, white)" strokeWidth="3" x1="43.1557" x2="22.1557" y1="1.08621" y2="21.0863" />
+            <line id="Line 100" stroke="var(--stroke-0, white)" strokeWidth="3" x1="515.121" x2="42.1212" y1="1.5" y2="1.50003" />
+            <line id="Line 101" stroke="var(--stroke-0, white)" strokeWidth="3" x1="988.121" x2="515.121" y1="1.5" y2="1.5" />
+            <line id="Line 97" stroke="var(--stroke-0, white)" strokeWidth="3" x1="348.182" x2="362.182" y1="1.93934" y2="15.9393" />
+            <line id="Line 98" stroke="var(--stroke-0, white)" strokeWidth="3" x1="668.061" x2="682.061" y1="15.9393" y2="1.93934" />
+            <line id="Line 93" stroke="var(--stroke-0, white)" strokeWidth="3" x1="514.061" x2="523.253" y1="24.9393" y2="15.747" />
+            <line id="Line 92" stroke="var(--stroke-0, white)" strokeWidth="3" x1="506.99" x2="516.182" y1="15.747" y2="24.9393" />
+            <line id="Line 96" stroke="var(--stroke-0, white)" strokeWidth="3" x1="361.121" x2="669.121" y1="15.4966" y2="15.5" />
+            <line id="Line 91" stroke="var(--stroke-0, white)" strokeWidth="3" x1="22.6212" x2="22.6212" y1="249" y2="20" />
+            <line id="Line 90" stroke="var(--stroke-0, white)" strokeWidth="3" x1="1007.62" x2="1007.62" y1="20" y2="249" />
+            <line id="Line 84" stroke="var(--stroke-0, white)" strokeWidth="3" x1="1007.18" x2="1029.18" y1="247.941" y2="269.939" />
+            <line id="Line 83" stroke="var(--stroke-0, white)" strokeWidth="3" x1="1.06064" x2="23.0605" y1="269.939" y2="247.942" />
+            <line id="Line 79" stroke="var(--stroke-0, white)" strokeWidth="3" x1="1029.18" x2="1007.18" y1="268.061" y2="290.058" />
+            <line id="Line 82" stroke="var(--stroke-0, white)" strokeWidth="3" x1="23.0591" x2="1.06066" y1="290.059" y2="268.061" />
+            <line id="Line 2" stroke="var(--stroke-0, white)" strokeWidth="3" x1="22.6212" x2="22.6212" y1="518" y2="289" />
+            <line id="Line 17" stroke="var(--stroke-0, white)" strokeWidth="3" x1="1007.62" x2="1007.62" y1="289" y2="518" />
+            <line id="Line 53" stroke="var(--stroke-0, white)" strokeWidth="3" x1="523.253" x2="514.061" y1="522.253" y2="513.061" />
+            <line id="Line 74" stroke="var(--stroke-0, white)" strokeWidth="3" x1="516.182" x2="506.99" y1="513.061" y2="522.253" />
+            <line id="Line 40" stroke="var(--stroke-0, white)" strokeWidth="3" x1="22.1557" x2="43.1557" y1="516.914" y2="536.914" />
+            <line id="Line 34" stroke="var(--stroke-0, white)" strokeWidth="3" x1="987.087" x2="1008.09" y1="536.914" y2="516.914" />
+            <line id="Line 54" stroke="var(--stroke-0, white)" strokeWidth="3" x1="669.121" x2="361.121" y1="522.503" y2="522.5" />
+            <line id="Line 47" stroke="var(--stroke-0, white)" strokeWidth="3" x1="682.061" x2="668.061" y1="536.061" y2="522.061" />
+            <line id="Line 55" stroke="var(--stroke-0, white)" strokeWidth="3" x1="362.182" x2="348.182" y1="522.061" y2="536.061" />
+            <line id="Line 35" stroke="var(--stroke-0, white)" strokeWidth="3" x1="515.121" x2="988.121" y1="536.5" y2="536.5" />
+            <line id="Line 39" stroke="var(--stroke-0, white)" strokeWidth="3" x1="42.1212" x2="515.121" y1="536.5" y2="536.5" />
+          </g>
       </svg>
       <div className="relative h-full z-10 p-12">{children}</div>
     </div>
