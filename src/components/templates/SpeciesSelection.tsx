13072496f0bf7dbcc8b3b863080aa1da0f12e5c0
Thomas Carreau <thomas.carreau@reseau.eseo.fr>
2025-12-10 15:35:19 +0100

    Corrections pour le déploiement

--- src/components/templates/SpeciesSelection.tsx
+++ src/components/templates/SpeciesSelection.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { SPECIES } from "../../data/species"
 import type { Species } from "../../types/data/species"
 import Title from "../atoms/Title"
 
@@ -8,6 +9,7 @@ export default function SpeciesSelection({ setSpecies }: { setSpecies: (species:
         <main className="h-screen w-screen flex flex-col items-center text-white p-6 bg-[url(/background_scale.png)] bg-cover overflow-auto">
             <Title name="Sélection de l'espèce" />
             <div>Species selection component - to be implemented</div>
+            <button onClick={() => setSpecies(SPECIES[0])}>Mettre une espèce de test</button>
         </main>
     )
 }
